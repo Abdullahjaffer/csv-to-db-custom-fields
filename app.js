46ae8af49902a9a56a20b8ff0ea8383b1a30da52
Abdullah <abdullahjaffer8@bitbucket.org>
2020-08-21 09:44:52 +0500

    initial

---
+++ app.js
@@ -0,0 +1,186 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+const { v4: uuidv4 } = require("uuid");
+const excelToJson = require("convert-excel-to-json");
+const fs = require("fs");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + "/uploads/");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
+const upload = multer({
+    storage: storage,
+});
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test2",
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+
+    connection.query(
+        `CREATE TABLE IF NOT EXISTS sheet_ids(id varchar(36) PRIMARY KEY, catalog_type varchar(200) NOT NULL, area varchar(200) , times_allowed_read int);`,
+        (err) => {
+            if (err) {
+                console.error("error creating table: " + err.stack);
+                return;
+            }
+        }
+    );
+    connection.query(
+        `CREATE TABLE IF NOT EXISTS sheet_data( id INT AUTO_INCREMENT PRIMARY KEY, date varchar(200), type varchar(200) NOT NULL , price varchar(200), ref varchar(36) );`,
+        (err) => {
+            if (err) {
+                console.error("error creating table: " + err.stack);
+                return;
+            }
+        }
+    );
+});
+
+app.use(express.static("build"));
+
+app.get("/get/:id", (req, res) => {
+    let sql = `SELECT * FROM sheet_ids WHERE id = '${req.params.id}'`;
+    console.log(sql);
+    console.log(req.params);
+    connection.query(sql, (err, data) => {
+        if (err) console.log(err);
+        else {
+            if (data.length > 0) {
+                if (data[0].times_allowed_read > 0) {
+                    let sql = `SELECT * FROM sheet_data WHERE ref = '${req.params.id}'`;
+                    connection.query(sql, (err, data) => {
+                        if (err) console.log(err);
+                        res.send({
+                            success: true,
+                            message: data,
+                        });
+                    });
+                    sql = `UPDATE sheet_ids SET times_allowed_read=${
+                        data[0].times_allowed_read - 1
+                    }  WHERE id = '${req.params.id}'`;
+                    connection.query(sql, (err) => {
+                        if (err) console.log(err);
+                        console.log("Decreased allowed times to read");
+                    });
+                } else {
+                    res.send({
+                        success: false,
+                        message: "Maximum downloads exceeded",
+                    });
+                }
+            } else {
+                res.send({
+                    success: false,
+                    message: "No such file",
+                });
+            }
+        }
+    });
+});
+
+app.post("/", upload.single("file"), (req, res, next) => {
+    const file = req.file;
+    if (!file) {
+        return res.send("Please upload a file");
+    }
+    const result = excelToJson({
+        sourceFile: __dirname + "/uploads/" + file.filename,
+    }).Sheet1;
+    let firstPart = result.splice(0, 4);
+    if (!firstPart[0]["B"]) {
+        errors.push(`Catalog type is required on B1`);
+    }
+    let area = firstPart[1]["B"];
+    let data = [];
+    let errors = [];
+    if (!errors.length) {
+        result.map((c, i) => {
+            let date = undefined;
+            if (c["A"]) {
+                date = c["A"];
+                delete c["A"];
+            }
+            Object.keys(c).map((d, i) => {
+                let type = firstPart[3][d];
+                let price = c[d];
+                if (price && isNaN(price)) {
+                    errors.push(
+                        `${price} is not a number on line ${d} ${i + 4}`
+                    );
+                }
+                data.push({
+                    date,
+                    type,
+                    price,
+                });
+            });
+        });
+    }
+    if (errors.length) {
+        return res.send({
+            success: false,
+            message: errors,
+        });
+    }
+    let id = "";
+    if (!errors.length) {
+        id = uuidv4();
+        var sql =
+            "INSERT INTO sheet_ids (id, catalog_type, area,  times_allowed_read) VALUES ?";
+        var values = [[id, firstPart[0]["B"], firstPart[1]["B"], 5]];
+        connection.query(sql, [values], function (err) {
+            if (err) {
+                console.log(err);
+                return res.send({
+                    success: false,
+                    message: "Failed to add data to database",
+                });
+            }
+            var sql =
+                "INSERT INTO sheet_data (date, type, price ,ref) VALUES ?";
+            var values = data.map((c) => [...Object.values(c), id]);
+            connection.query(sql, [values], function (err) {
+                if (err) {
+                    console.log(err);
+                    return res.send({
+                        success: false,
+                        message: "Failed to add data to database",
+                    });
+                }
+                return res.send({
+                    success: true,
+                    message: "Data added successfully.",
+                    id: id,
+                });
+            });
+        });
+    }
+
+    try {
+        fs.unlinkSync(__dirname + "/uploads/" + file.filename);
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("listening on ", process.env.PORT || 5000);
+});
---
+++ build/precache-manifest.596e52dc84c69e4a9f3fe6c7cc5554c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3058b0562ffd73f1c0315db6f6bf305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a38e56d21499380c7206",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "d9b3c7c71c4becd8c21a",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "a38e56d21499380c7206",
+    "url": "/static/js/2.cc0f73f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cc0f73f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b3c7c71c4becd8c21a",
+    "url": "/static/js/main.396612a1.chunk.js"
+  },
+  {
+    "revision": "e728199d135f32a8fe0d",
+    "url": "/static/js/runtime-main.272664ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
